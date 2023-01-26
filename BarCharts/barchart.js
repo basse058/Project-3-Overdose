3b95fadbac812ae80da0fd93d1c2aac7f3bfd521
ChristyGruen <chrisgruen@yahoo.com>
2023-01-25 20:05:06 -0600

    update barchart.js file

--- BarCharts/barchart.js
+++ BarCharts/barchart.js
@@ -67,62 +67,62 @@ function barChartPres(dataP) {
 });
 
 
-// const dataPromise2 = d3.json(urlDeaths);
-// console.log("Data Promise: ", dataPromise2);
+const dataPromise2 = d3.json(urlDeaths);
+console.log("Data Promise: ", dataPromise2);
 
-// console.log(stateAbb);
+console.log(stateAbb);
 
-// d3.json(urlDeaths).then(function(dataD) {
+d3.json(urlDeaths).then(function(dataD) {
 
-//     console.log(dataD.AK[0]);
-//     console.log(dataD.AK[3]);
-//     console.log(dataD['Opioid Dispense Rate'][0]);
-//     console.log(dataD['Opioid Dispense Rate'][3]);
+    console.log(dataD.AK[0]);
+    console.log(dataD.AK[3]);
+    console.log(dataD['Number_of_Deaths_by_Year'][0]);
+    console.log(dataD['Number_of_Deaths_by_Year'][3]);
 
-//   let state = dataD[stateAbb]
-//     console.log(state)
+  let state = dataD[stateAbb]
+    console.log(state)
 
-// function barChartPres(dataP) {
-//     let bary = [state[0],state[1],state['2'],state['3'],state['4'],state['5'],state['6']];
+function barChartDeaths(dataD) {
+    let bary = [state[0],state[1],state['2'],state['3'],state['4'],state['5'],state['6']];
 
-//     // for (let i=0; i<7; i++) {
-//     //   bary.append(data[nameIndex][i])
-//     // };
-//     // for (dnI in data.nameIndex) {
-//     //   bary.append(data.nameIndex[dnI])
-//     // };//sample_values
-//     let barx = dataP['Opioid Dispense Rate'];//otu_ids, use this to build string for chart
-//     // let barystr = []// use this for bar chart
-//     // bary.forEach(pear => barystr.push(`OTU_${pear}`));
-//     let barz = dataP['Opioid Dispense Rate'];//otu_labels
-//     console.log(barx)
-//     console.log(bary)
-//     // console.log(barz)
-//     //color options
-//     let colorlist = [ 'red','orangered','orange', 'yellow','yellowgreen', 'green','blue','mediumblue','rebeccapurple','indigo'];//pairs with Jet and Portland
-//     let colorlist2 = ['#f0f921','#fdca26','#fb9f3a','#ed7953','#d8576b','#bd3786','#9c179e','#7201a8','#46039f','#0d0887']//reverse plasma
+    // for (let i=0; i<7; i++) {
+    //   bary.append(data[nameIndex][i])
+    // };
+    // for (dnI in data.nameIndex) {
+    //   bary.append(data.nameIndex[dnI])
+    // };//sample_values
+    let barx = dataD['Number_of_Deaths_by_Year'];//otu_ids, use this to build string for chart
+    // let barystr = []// use this for bar chart
+    // bary.forEach(pear => barystr.push(`OTU_${pear}`));
+    let barz = dataD['Number_of_Deaths_by_Year'];//otu_labels
+    console.log(barx)
+    console.log(bary)
+    // console.log(barz)
+    //color options
+    let colorlist = [ 'red','orangered','orange', 'yellow','yellowgreen', 'green','blue','mediumblue','rebeccapurple','indigo'];//pairs with Jet and Portland
+    let colorlist2 = ['#f0f921','#fdca26','#fb9f3a','#ed7953','#d8576b','#bd3786','#9c179e','#7201a8','#46039f','#0d0887']//reverse plasma
 
-//     let trace1 = {
-//         x: barx,
-//         y: bary,
-//         text: barz,
-//         type: 'bar',
-//         orientation: 'v',
-//         marker:{color: colorlist2}
-//       };
-//       let data1 = [trace1];
+    let trace1 = {
+        x: barx,
+        y: bary,
+        text: barz,
+        type: 'bar',
+        orientation: 'v',
+        marker:{color: colorlist2}
+      };
+      let data1 = [trace1];
       
-//       let layout1 = {
-//         // title: {text: `ID_${data.names[nameIndex]} Top 10 OTUs<br><sup>(Operational Taxonomic Units)</sup>`,font: { size: 24 } },
-//         showlegend: false,
-//         height: 400,
-//         width: 500,
-//         xaxis: {title:"Opioid Dispense Rate"},
-//         margin: { t: 50, r: 25, l: 75, b: 35},
-//         paper_bgcolor: "aliceblue",
-//         font: { color: "darkblue", family: "Arial" }
-//       };
+      let layout1 = {
+        // title: {text: `ID_${data.names[nameIndex]} Top 10 OTUs<br><sup>(Operational Taxonomic Units)</sup>`,font: { size: 24 } },
+        showlegend: false,
+        height: 400,
+        width: 500,
+        xaxis: {title:"Number of Deaths per Year"},
+        margin: { t: 50, r: 25, l: 75, b: 35},
+        paper_bgcolor: "aliceblue",
+        font: { color: "darkblue", family: "Arial" }
+      };
 
-//       Plotly.newPlot("gauge", data1,layout1)};
-//   barChartPres(state);
-// });
+      Plotly.newPlot("gauge", data1,layout1)};
+  barChartDeaths(state);
+});
